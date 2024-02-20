d77cc582bf12e772f6222a3e35b329b275d72b91
phuocdz5 <phuoccntt2004@gmail.com>
2024-02-20 17:28:05 +0700

    update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 const UserModel = require("../models/userModel");
-const bcryp = require('bcryptjs');
+const bcryp = require('/node_modules/bcrypt');
 const asyncHandle = require('express-async-handler');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
