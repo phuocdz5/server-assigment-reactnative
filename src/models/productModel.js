d6974fd4fe21489dbf9ceced377c3c3133aa96c4
phuocdz5 <phuoccntt2004@gmail.com>
2024-02-22 21:54:57 +0700

    update

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,34 +1,53 @@
 const mongoose = require('mongoose');
 
-const ProductSchema = new mongoose.Schema({
+const PriceSchema = new mongoose.Schema({
+    size: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    currency: {
+      type: String,
+      required: true,
+      default: '$', // Nếu có thể, bạn có thể đặt giá trị mặc định là '$' hoặc làm cho trường này không bắt buộc.
+    },
+  });
+  
+  const ProductSchema = new mongoose.Schema({
     name: {
-        type: String,
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
+      type: String,
+      required: true,
     },
     roasted: {
-        type: String,
+      type: String,
     },
     imagelink_square: {
-        type: String,
+      type: String,
     },
     imagelink_portrait: {
-        type: String,
+      type: String,
     },
-    price:{
-        type:String,
+    prices: {
+      type: [PriceSchema],
+      required: true,
     },
-    size:{
-        type:String,
+    type: {
+      type: String,
+      required: true,
     },
-    type:{
-        type:String,
-    }
-
-    
-});
-
-const ProductModel = mongoose.model('product', ProductSchema);
-
-module.exports = ProductModel;
+    index: {
+      type: Number,
+      required: true,
+    },
+  });
+  
+  const ProductModel = mongoose.model('product', ProductSchema);
+  
+  module.exports = ProductModel;
