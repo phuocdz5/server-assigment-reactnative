1e9e43df196c7e3b9bc03e58810ebf953652013e
phuocdz5 <phuoccntt2004@gmail.com>
2024-02-22 16:09:53 +0700

    update

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express');
-const { register, login, verification } = require('../controllers/authController');
+const { register, login, verification, forgotPassword } = require('../controllers/authController');
 
 const authRouter = Router();
 
 authRouter.post('/register', register)
 authRouter.post('/login', login)
 authRouter.post('/verification', verification)
+authRouter.post('/forgotPassword',forgotPassword)
 module.exports = authRouter;
